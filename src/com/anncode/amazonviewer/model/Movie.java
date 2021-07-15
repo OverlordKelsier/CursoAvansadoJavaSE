c714f3647b31343e342572b52957e751fb36975e
Overlord <leon.pa.adolfo@gmail.com>
2021-07-15 13:52:58 -0500

    Programas subidos a github (Amazonviewer y Lamdas)

--- src/com/anncode/amazonviewer/model/Movie.java
+++ src/com/anncode/amazonviewer/model/Movie.java
@@ -3,12 +3,20 @@ package com.anncode.amazonviewer.model;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Movie extends Film implements IVisualizable {
+import com.anncode.amazonviewer.dao.MovieDAO;
+
+/**
+ * Hereda de {@link Film}
+ * Implementea de {@link IVisualizable}
+ * */
+public class Movie extends Film implements IVisualizable, MovieDAO {
 	
 	private int id;
 	private int timeViewed;
 	
-	
+	public Movie() {
+		
+	}
 	public Movie(String title, String genre, String creator, int duration, short year) {
 		super(title, genre, creator, duration);
 		setYear(year);
@@ -18,7 +26,9 @@ public class Movie extends Film implements IVisualizable {
 	public int getId() {
 		return id;
 	}
-	
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 	public int getTimeViewed() {
 		return timeViewed;
@@ -37,13 +47,19 @@ public class Movie extends Film implements IVisualizable {
 				"\n Creator: " + getCreator() +
 				"\n Duration: " + getDuration();
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 * */
 	@Override
 	public Date startToSee(Date dateI) {
 		// TODO Auto-generated method stub
 		return dateI;
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 * */
 	@Override
 	public void stopToSee(Date dateI, Date dateF) {
 		// TODO Auto-generated method stub
@@ -58,13 +74,32 @@ public class Movie extends Film implements IVisualizable {
 	}
 	
 	public static ArrayList<Movie> makeMoviesList() {
-		ArrayList<Movie> movies = new ArrayList();
+		Movie  movie = new Movie();
+		return movie.read();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * */
+	@Override
+	public void view() {
+		// TODO Auto-generated method stub
+		setViewed(true);
+		Movie movie = new Movie();
+		movie.setMovieViewed(this);
+				
+		Date dateI = startToSee(new Date());
 		
-		for (int i = 1; i <= 5; i++) {
-			movies.add(new Movie("Movie " + i, "Genero " + i, "Creador " + i, 120+i, (short)(2017+i)));
+		
+		for (int i = 0; i < 100000; i++) {
+			System.out.println("..........");
 		}
 		
-		return movies;
+		//Termine de verla
+		stopToSee(dateI, new Date());
+		System.out.println();
+		System.out.println("Viste: " + toString());
+		System.out.println("Por: " + getTimeViewed() + " milisegundos");
 	}
 	
 }
